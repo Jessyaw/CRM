8829edd477a31bcdb22d26d36b13bc1e9ee109df
JessyAngel07 <144873346+JessyAngel07@users.noreply.github.com>
2025-10-08 14:14:58 +0530

    jc

--- obj/Release/net8.0/CRM.AssemblyInfo.cs
+++ obj/Release/net8.0/CRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4a9bb84ffbd64a152dfe6bb7f558946ef0980b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3141bf8a3ad28d8ecdb5263ac51c7f99bb5ab079")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
